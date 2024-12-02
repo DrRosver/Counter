d409c9557f2270f0343a15ea83f34d24ef4729e2
filip <filip@PC>
2024-12-02 23:52:39 +0100

    Dodaj pliki projektów.

---
+++ Counter/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Counter.Views;
+
+public partial class MainPage : ContentPage
+{
+    private int _counter = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void OnIncrementClicked(object sender, EventArgs e)
+    {
+        _counter++; 
+        CounterLabel.Text = _counter.ToString(); 
+    }
+
+    private void OnDecrementClicked(object sender, EventArgs e)
+    {
+        _counter--; 
+        CounterLabel.Text = _counter.ToString(); 
+    }
+}
